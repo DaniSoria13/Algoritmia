fd60465942f2ff17c6abeeab83b740da28868dd3
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 17:20:03 +0200

    Creamos la función resolver para que el usuario pueda mover los discos

--- TorresHanoi/Hanoi.java
+++ TorresHanoi/Hanoi.java
@@ -1,4 +1,13 @@
 package TorresHanoi;
 
 public class Hanoi {
+    public void resolver(int n, char origen, char auxiliar, char destino) {
+        if (n == 1) {
+            System.out.println("Mover disco 1 de " + origen + " a " + destino);
+            return;
+        }
+        resolver(n - 1, origen, destino, auxiliar);
+        System.out.println("Mover disco " + n + " de " + origen + " a " + destino);
+        resolver(n - 1, auxiliar, origen, destino);
+    }
 }
