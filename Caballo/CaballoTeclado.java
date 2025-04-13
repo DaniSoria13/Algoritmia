fe49649509f60aaf2313a62268d6d4c037c8067b
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 18:47:37 +0200

    Creamos el contador de los movimientos

--- Caballo/CaballoTeclado.java
+++ Caballo/CaballoTeclado.java
@@ -16,4 +16,29 @@ public class CaballoTeclado {
             {2, 4}      // 9
     };
 
+    public int contarMovimientos(int n) {
+        // dp[i][j] representa la cantidad de formas de llegar al número j con i movimientos
+        int[][] dp = new int[n + 1][10];
+
+        // Inicializar: con 0 movimientos hay una forma de estar en cada dígito
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int paso = 1; paso <= n; paso++) {
+            for (int num = 0; num <= 9; num++) {
+                dp[paso][num] = 0; // Reiniciar
+                for (int next : movimientos[num]) {
+                    dp[paso][num] += dp[paso - 1][next];
+                }
+            }
+        }
+
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += dp[n][i];
+        }
+
+        return total;
+    }
 }
