19ccc0d7201bf1d0e8ccb3c899879e81b09ec19b
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 18:25:12 +0200

    Creamos las funciones para resolver el juego

--- Reinas/NReinas.java
+++ Reinas/NReinas.java
@@ -5,9 +5,28 @@ public class NReinas {
     private int N;
     private int[][] tablero;
 
-    public NReinas(int n) {
+    public void nReinas(int n) {
         this.N = n;
         tablero = new int[N][N];
     }
+    public boolean resolver() {
+        return resolverUtil(0);
+    }
 
+    private boolean resolverUtil(int col) {
+        if (col >= N) {
+            imprimirTablero();
+            return true;
+        }
+
+        boolean res = false;
+        for (int i = 0; i < N; i++) {
+            if (esSeguro(i, col)) {
+                tablero[i][col] = 1;
+                res = resolverUtil(col + 1) || res;
+                tablero[i][col] = 0;
+            }
+        }
+        return res;
+    }
 }
