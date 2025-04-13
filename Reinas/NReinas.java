11708ad529841e9db3008a3621026526d81bb8cc
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 18:26:27 +0200

    Creamos la funcion si es seguro

--- Reinas/NReinas.java
+++ Reinas/NReinas.java
@@ -29,4 +29,20 @@ public class NReinas {
         }
         return res;
     }
+
+    private boolean esSeguro(int fila, int col) {
+        for (int i = 0; i < col; i++)
+            if (tablero[fila][i] == 1)
+                return false;
+
+        for (int i = fila, j = col; i >= 0 && j >= 0; i--, j--)
+            if (tablero[i][j] == 1)
+                return false;
+
+        for (int i = fila, j = col; i < N && j >= 0; i++, j--)
+            if (tablero[i][j] == 1)
+                return false;
+
+        return true;
+    }
 }
