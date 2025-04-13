ec47839c93a21d10287de6f92a1d0e7d40e17ede
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 17:52:25 +0200

    Creamos la función con la que se mostrarán las torres

--- TorresHanoi/Hanoi.java
+++ TorresHanoi/Hanoi.java
@@ -6,10 +6,23 @@ public class Hanoi {
 
     public void torresdehanoi() {
         // Colocar los discos en la torre A (columna 0), desde la base hacia arriba
-        torres[0][0] = 3;
+        torres[0][0] = 1;
         torres[1][0] = 2;
-        torres[2][0] = 1;
+        torres[2][0] = 3;
     }
 
+    private void mostrarTorres() {
+        System.out.println("Estado actual:");
+        for (int fila = 0; fila < 3; fila++) {
+            for (int col = 0; col < 3; col++) {
+                int disco = torres[fila][col];
+                System.out.print(" " + (disco == 0 ? "." : disco));
+            }
+            System.out.println();
+        }
+        System.out.println("---------");
+        System.out.println("  A B C\n");
+    }
+}
 
 }
