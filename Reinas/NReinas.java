ca599ffc58ab9c386123e7cb053885dec972e4f6
DaniSoria13 <dgarcsor@myuax.com>
2025-04-13 18:18:53 +0200

    Creamos la variable N y el trablero

---
+++ Reinas/NReinas.java
@@ -0,0 +1,13 @@
+package Reinas;
+
+public class NReinas {
+
+    private int N;
+    private int[][] tablero;
+
+    public NReinas(int n) {
+        this.N = n;
+        tablero = new int[N][N];
+    }
+
+}
