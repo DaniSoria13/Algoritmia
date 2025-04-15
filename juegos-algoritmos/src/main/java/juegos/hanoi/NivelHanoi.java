17202bcba67a49fc5c9e8ea183cd12a8eb6cc7fe
DaniSoria13 <dgarcsor@myuax.com>
2025-04-15 12:35:10 +0200

    Desarrollamos la lógica para seleccionar el nivel y obtener el número de discos

--- juegos-algoritmos/src/main/java/juegos/hanoi/NivelHanoi.java
+++ juegos-algoritmos/src/main/java/juegos/hanoi/NivelHanoi.java
@@ -1,4 +1,17 @@
 package juegos.hanoi;
 
 public class NivelHanoi {
+    private int nivel;
+
+    public NivelHanoi(int nivel) {
+        if (nivel < 1 || nivel > 5) {
+            this.nivel = 1;
+        } else {
+            this.nivel = nivel;
+        }
+    }
+
+    public int getNumeroDiscos() {
+        return 2 + nivel; // Nivel 1 = 3 discos, Nivel 2 = 4, ..., Nivel 5 = 7
+    }
 }
