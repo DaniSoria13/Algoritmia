3af3e66dffd3fc67a5f7166605153df8bdb2f444
DaniSoria13 <dgarcsor@myuax.com>
2025-04-15 12:10:53 +0200

    Cambiamos el main para que el usuario pueda decidir a que juego quiere jugar

--- juegos-algoritmos/src/main/java/juegos/Main.java
+++ juegos-algoritmos/src/main/java/juegos/Main.java
@@ -6,13 +6,34 @@ import juegos.caballo.MovimientoCaballo;
 
 public class Main {
     public static void main(String[] args) {
-        TorresDeHanoi juegoHanoi = new TorresDeHanoi();
-        juegoHanoi.jugar();
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int opcion;
 
-        NReinas juegoReinas = new NReinas();
-        juegoReinas.jugar();
+        do {
+            System.out.println("\n--- Menú de Juegos ---");
+            System.out.println("1. Torres de Hanoi");
+            System.out.println("2. N Reinas");
+            System.out.println("3. Movimiento del Caballo");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
 
-        MovimientoCaballo juegoCaballo = new MovimientoCaballo();
-        juegoCaballo.jugar();
+            switch (opcion) {
+                case 1 -> {
+                    TorresDeHanoi juegoHanoi = new TorresDeHanoi();
+                    juegoHanoi.jugar();
+                }
+                case 2 -> {
+                    NReinas juegoReinas = new NReinas();
+                    juegoReinas.jugar();
+                }
+                case 3 -> {
+                    MovimientoCaballo juegoCaballo = new MovimientoCaballo();
+                    juegoCaballo.jugar();
+                }
+                case 0 -> System.out.println("Saliendo del programa...");
+                default -> System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        } while (opcion != 0);
     }
 }
